4a80fbf33974bb12d6a68f57f40647692e37d610
Rohit Trivedi <86.rohit@gmail.com>
2024-09-08 00:05:00 +0530

    init project using npx create-rakkas-app@next rakkas-convex

---
+++ src/routes/todo/index.page.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { Todo } from "./Todo";
+import css from "./page.module.css";
+import {
+  Page,
+  queryOptions,
+  runServerSideQuery,
+  useQuery,
+  useQueryClient,
+  useServerSideMutation,
+} from "rakkasjs";
+
+import { createTodo, readAllTodos } from "src/crud";
+
+const TodoPage: Page = () => {
+  const { data } = useQuery(todos);
+
+  const [text, setText] = useState("");
+
+  const client = useQueryClient();
+
+  const { mutate: create } = useServerSideMutation(
+    async () => {
+      createTodo({ text, done: false });
+    },
+    {
+      onSuccess() {
+        client.invalidateQueries("todos");
+      },
+    },
+  );
+
+  return (
+    <main>
+      <h1>Todo</h1>
+
+      <p>This is a simple todo application that demonstrates data fetching.</p>
+
+      <ul className={css.todoList}>
+        {data.map((todo) => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
+      </ul>
+
+      <p className={css.p}>
+        <input
+          className={css.input}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+
+        <button
+          type="button"
+          className={css.addButton}
+          disabled={!text}
+          onClick={() => create()}
+        >
+          Add
+        </button>
+      </p>
+    </main>
+  );
+};
+
+export default TodoPage;
+
+TodoPage.preload = (ctx) => {
+  ctx.queryClient.prefetchQuery(todos);
+};
+
+const todos = queryOptions({
+  queryKey: "todos",
+  queryFn(ctx) {
+    return runServerSideQuery(ctx.requestContext, readAllTodos);
+  },
+  refetchOnWindowFocus: true,
+  refetchOnReconnect: true,
+});
