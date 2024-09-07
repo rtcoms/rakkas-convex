0ce5f7ecd0b4be853fb3a4a602df4e44ec962497
Rohit Trivedi <86.rohit@gmail.com>
2024-09-08 00:21:05 +0530

    update config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 import rakkas from "rakkasjs/vite-plugin";
 
+const env = loadEnv("", process.cwd(), "");
+Object.assign(process.env, env);
+
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), rakkas()],
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    rakkas({
+      // This is required because of a Rakkas bug. Expect a fix this week
+      serverOnlyFiles: {
+        exclude: ["**/node_modules/**"],
+      },
+    }),
+  ],
+  build: {
+    sourcemap: true,
+  },
 });
